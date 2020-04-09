f11cfab1448c4f69b5d0c4fb58070109d88fa5b7
Alex <lafouine.becs@gmail.com>
2020-04-09 14:24:55 +0200

    Amélioration des performances coté serveur
    Modification de la boucle de mise à jour des possesseurs de Bracelet GPS.

--- server/main.lua
+++ server/main.lua
@@ -2,49 +2,48 @@
 -- Développé par Fouinette
 -- Pour le projet MyCitY RP
 --================================
-ESX	= nil;
-local attente = 0;
+ESX		= nil;
+Porteur	= {};
 
 -- Initialisation du FrameWork
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+Citizen.CreateThread(function()
+	while true do
+		local xPlayers	= ESX.GetPlayers();
+		Porteur	= {};
+
+		for i=1, #xPlayers, 1 do
+			local xPlayer = ESX.GetPlayerFromId(xPlayers[i]);
+
+			for k,v in ipairs(Config.items) do
+				if xPlayer.getInventoryItem(v).count > 0 then
+					table.insert(Porteur, xPlayer);
+					break;
+				end
+			end
+		end
+		Citizen.Wait(30000);
+	end
+end)
 
 -- Récupération de la liste des joueurs avec Bracelets.
 RegisterNetEvent('esx_braceletgps:srv_updateliste')
 AddEventHandler('esx_braceletgps:srv_updateliste', function()
 	local sPlayer	= ESX.GetPlayerFromId(source);
-	local xPlayers	= ESX.GetPlayers();
-	local retour	= {};
-
-	for i=1, #xPlayers, 1 do
-	    local xPlayer = ESX.GetPlayerFromId(xPlayers[i]);
-
-	    for k,v in ipairs(Config.items) do
-		    if xPlayer.getInventoryItem(v).count > 0 then
-			    table.insert(retour, xPlayer);
-			    break;
-		    end
-	    end
-	end
-
-	TriggerClientEvent('esx_braceletgps:majbracelets', sPlayer.source, retour);
-end)
-
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(15000)
-		attente = 0;
-	end
+	TriggerClientEvent('esx_braceletgps:majbracelets', sPlayer.source, Porteur);
 end)
 
 ESX.RegisterUsableItem('braceletgps', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source);
 	local data = {};
 	
-	if attente == 0 then
-		attente = 1;
-		data.message = "ALERTE BRACELET GPS";
-		data.number = "police";
+	data.message = "ALERTE BRACELET GPS";
+	-- Envoi du message à tous les metiers autorisés.
+	for _,v in ipairs(Config.metiers) do
+		data.number = v;
 		TriggerClientEvent('esx_addons_gcphone:call', source, data);
-		TriggerEvent('Fouinette_srv:logs', 'Envoi alerte Breacelet GPS par '.. tostring(xPlayer.identifier));
 	end
+	TriggerEvent('Fouinette_srv:logs', 'Envoi alerte Breacelt GPS par '.. tostring(xPlayer.identifier));
+	
 end)
